64af4ad116028299f02afcdddcbef50508e9844c
Thanyaporn <mod5832030008@gmail.com>
2018-05-09 12:26:21 +0700

    studentApp

--- src/LAB5/arryOperator.java
+++ src/LAB5/arryOperator.java
@@ -4,45 +4,45 @@ import java.util.Scanner;
 
 //1. method for input data  (10)
 //2. method for show data in array
-//3. method to find average value in array
-//4. method to find maximum value in array
-//5. method to find minimum value in array
-public class arrayOperator {
+//3. method to find average value in arry
+//4. method to find maximum value in arry
+//5. method to find minimum value in arry
+public class arryOperator {
     public static final int MAX = 10;
     public static void main(String[] args) {
-        int myArray[] = new int[MAX];
-        myArray = inputData(myArray);
-        showData(myArray);
+        int myarray[] = new int[MAX];
+        myarray = inputData(myarray);
+        showData(myarray);
         //find average
-        findAverage(myArray);
+        findAverage(myarray);
         //find Max
-        findMax(myArray);
+        findMax(myarray);
         //find min
-        findmin(myArray);
+        findmin(myarray);
 
     }  //main
 
-    private static void findmin(int[] myArray) {
-        int min = myArray[0];
-        for (int i = 0; i < myArray.length; i++) {
-            if (min>= myArray[i])
-                min= myArray[i];
+    private static void findmin(int[] myarray) {
+        int min = myarray[0];
+        for (int i = 0; i < myarray.length; i++) {
+            if (min>= myarray[i])
+                min= myarray[i];
         }
         System.out.println("The maximum value is "+min);
     }
 
-    private static void findMax(int[] myArray) {
-        int max = myArray[0];
-        for (int i = 0; i < myArray.length; i++) {
-            if (max<=myArray[i])
-                max = myArray[i];
+    private static void findMax(int[] myarray) {
+        int max = myarray[0];
+        for (int i = 0; i < myarray.length; i++) {
+            if (max<=myarray[i])
+                max = myarray[i];
         }
         System.out.println("The maximum value is "+max);
     }
 
-    private static void findAverage(int[] myArray) {
+    private static void findAverage(int[] myarray) {
         int total = 0;
-        for (int val:myArray)
+        for (int val:myarray)
             total += val;
         System.out.println("The average value is "+(total/MAX));
     }
