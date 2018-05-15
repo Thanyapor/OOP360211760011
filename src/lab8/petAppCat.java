9b4f42e1e4c6ae191b975e54e099b78dfa48e77e
Thanyaporn <mod5832030008@gmail.com>
2018-05-15 16:44:44 +0700

    Pat

---
+++ src/lab8/petAppCat.java
@@ -0,0 +1,13 @@
+package lab8;
+
+public class petAppCat {
+    public static void main(String[] args){
+
+        Cat d2 = new Cat();
+        d2.setName("Sandee");
+        d2.setAge(1);
+
+        System.out.println(d2.toString()+" "+d2.getClass().getName());
+
+    }//main
+}//class
